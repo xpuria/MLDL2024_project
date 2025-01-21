7eac3da588005383db13a197f3a78097f6bf2f03
pouria mohammadalipourahari <aharipuria1@gmail.com>
2025-01-21 19:12:31 +0100

    train modified

--- train.py
+++ train.py
@@ -98,9 +98,9 @@ def train_model(model: nn.Module,
                 images = images.to(device)
                 labels = labels.to(device)
                 
-                outputs, _, _ = model(images)
-                #if isinstance(outputs, tuple):
-                #    outputs = outputs[0]
+                outputs= model(images)
+                if isinstance(outputs, tuple):
+                    outputs = outputs[0]
                 
                 loss = criterion(outputs, labels)
                 val_loss += loss.item()
